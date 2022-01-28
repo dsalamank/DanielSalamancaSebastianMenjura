abb6fbe5b72f22908f1c1e404ebecb4a81bae3c2
dsalamank <98601932+dsalamank@users.noreply.github.com>
2022-01-28 16:16:01 -0500

    Update Tarea1.py

--- Semana1/Tarea1.py
+++ Semana1/Tarea1.py
@@ -3,3 +3,73 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 from tqdm import tqdm 
 import matplotlib.animation as anim
+class Particle():
+    
+    # init
+    def __init__(self, r0,v0,a0,t,m,radius,Id):
+        
+        self.dt  = t[1] - t[0]
+        
+        self.r = r0
+        self.v = v0
+        self.a = a0
+        
+        self.rVector = np.zeros( (len(t),len(r0)) )
+        self.vVector = np.zeros( (len(t),len(v0)) )
+        self.aVector = np.zeros( (len(t),len(a0)) )
+        
+        self.m = m
+        self.radius = radius
+        self.Id = Id
+        
+    # Method
+    def Evolution(self,i):
+        
+        self.SetPosition(i,self.r)
+        self.SetVelocity(i,self.v)
+        
+       # print(self.r)
+        
+        # Euler method
+        self.r += self.dt * self.v
+        self.v += self.dt * self.a
+    
+    def CheckWallLimits(self,limits,dim=2):
+        
+        for i in range(dim):
+            
+            if self.r[i] + self.radius > limits[i]:
+                self.v[i] = - self.v[i]
+            if self.r[i] - self.radius < - limits[i]:
+                self.v[i] = - self.v[i]
+    
+    # Setters
+    
+    def SetPosition(self,i,r):
+        self.rVector[i] = r
+        
+    def SetVelocity(self,i,v):
+        self.vVector[i] = v
+        
+    # Getters  
+    def GetPositionVector(self):
+        return self.rVector
+    
+    def GetRPositionVector(self):
+        return self.RrVector 
+    
+
+    def GetVelocityVector(self):
+        return self.vVector
+    
+    def GetR(self):
+        return self.radius
+    
+    def ReduceSize(self,factor):
+        
+        self.RrVector = np.array([self.rVector[0]]) # initial condition
+        
+        
+        for i in range(1,len(self.rVector)):
+            if i%factor == 0:
+                self.RrVector = np.vstack([self.RrVector,self.rVector[i]])
