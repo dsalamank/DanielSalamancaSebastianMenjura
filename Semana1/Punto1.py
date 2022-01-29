7f1684bffc1f0f05e4b879b2e6b1006d6ac05bd5
dsalamank <98601932+dsalamank@users.noreply.github.com>
2022-01-28 21:51:30 -0500

    Update and rename Tarea1.py to Punto1.py

---
+++ Semana1/Punto1.py
@@ -0,0 +1,144 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm 
+import matplotlib.animation as anim
+
+class Particle():
+    
+    # init
+    def __init__(self, r0,v0,a0,t,m,radius,Id):
+        
+        self.dt  = t[1] - t[0]
+        
+        self.r = r0
+        self.v = v0
+        self.a = a0
+        
+        self.rVector = np.zeros( (len(t),len(r0)) )
+        self.vVector = np.zeros( (len(t),len(v0)) )
+        self.aVector = np.zeros( (len(t),len(a0)) )
+        
+        self.m = m
+        self.radius = radius
+        self.Id = Id
+        
+    # Method
+    def Evolution(self,i):
+        
+        self.SetPosition(i,self.r)
+        self.SetVelocity(i,self.v)
+        
+       # print(self.r)
+        
+        # Euler method
+        self.r += self.dt * self.v
+        self.v += self.dt * self.a
+    
+    def CheckWallLimits(self,limits,dim=2):
+        
+        for i in range(dim):
+            
+            if self.r[i] + self.radius > limits[i]:
+                self.v[i] = - self.v[i]*0.9
+            if self.r[i] - self.radius < - limits[i]:
+                self.v[i] = - self.v[i]*0.9
+    
+    # Setters
+    
+    def SetPosition(self,i,r):
+        self.rVector[i] = r
+        
+    def SetVelocity(self,i,v):
+        self.vVector[i] = v
+        
+    # Getters  
+    def GetPositionVector(self):
+        return self.rVector
+    
+    def GetRPositionVector(self):
+        return self.RrVector 
+    
+
+    def GetVelocityVector(self):
+        return self.vVector
+    
+    def GetR(self):
+        return self.radius
+    
+    def ReduceSize(self,factor):
+        
+        self.RrVector = np.array([self.rVector[0]]) # initial condition
+        
+        
+        for i in range(1,len(self.rVector)):
+            if i%factor == 0:
+                self.RrVector = np.vstack([self.RrVector,self.rVector[i]])
+
+dt = 0.01
+tmax = 30
+t = np.arange(0,tmax+dt,dt)
+
+def GetParticle(Limit,Velo,Dim=2,dt=0.1):
+    
+    x0 = np.array([-15.,-5.])
+    v0 = np.array([1.,0.])
+    a0 = np.array([0,-9.8])
+        
+    p = Particle(x0,v0,a0,t,1.,1.0,i)
+        
+    return p
+Limits = np.array([20.,20.])
+
+def RunSimulation(t,NParticles = 100, Velo = 6):
+    
+    Particles = GetParticles(NParticles,Limits[0],Velo = Velo,dt=dt)
+    
+    for it in tqdm(range(len(t))): # Evolucion temporal
+        for i in range(len(Particles)):
+            
+            Particles[i].CheckWallLimits(Limits)
+            Particles[i].Evolution(it)
+        
+        
+    return Particles
+Particles = RunSimulation(t,1,Velo=6)
+def ReduceTime(t,factor):
+    
+    for p in Particles:
+        p.ReduceSize(factor)
+        
+    Newt = []
+    
+    for i in range(len(t)):
+        if i%factor == 0:
+            Newt.append(t[i])
+            
+    return np.array(Newt)
+redt = ReduceTime(t,10)
+fig = plt.figure(figsize=(5,5))
+ax = fig.add_subplot(1,1,1)
+
+
+def init():
+    ax.set_xlim(-Limits[0],Limits[0])
+    ax.set_ylim(-Limits[1],Limits[1])
+
+def Update(i):
+    
+    plot = ax.clear()
+    init()
+    plot = ax.set_title(r'$t=%.2f \ seconds$' %(redt[i]), fontsize=15)
+    
+    for p in Particles:
+        x = p.GetRPositionVector()[i,0]
+        y = p.GetRPositionVector()[i,1]
+        
+        vx = p.GetVelocityVector()[i,0]
+        vy = p.GetVelocityVector()[i,1]
+        
+        circle = plt.Circle( (x,y), p.GetR(), color='k', fill=False)
+        plot = ax.add_patch(circle)
+        plot = ax.arrow(x,y,vx,vy,color='r',head_width=0.5)
+        
+    return plot
+Animation = anim.FuncAnimation(fig,Update,frames=len(redt),init_func=init)
